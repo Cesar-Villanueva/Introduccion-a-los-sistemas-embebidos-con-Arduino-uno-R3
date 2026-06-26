758242e26008f71229377a6396973e29d3963748
Cesar-Villanueva <78750676+Cesar-Villanueva@users.noreply.github.com>
2026-06-26 11:12:42 -0600

    Add files via upload

---
+++ BloqueDeInformacion.ino
@@ -0,0 +1,16 @@
+/*
+Autor : Cesar Villanueva
+Date : 12/12/25
+Copyright : None
+Contac : cedeci . cvillanueva@ieca . edu . mx
+Changes by : None
+Fixes : None
+*/
+
+void setup () {
+
+  }
+
+ void loop () {
+
+ }
